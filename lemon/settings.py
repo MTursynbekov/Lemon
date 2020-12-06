76c87b0eb0f70e23b170e85d2a920e9f7793fdeb
Mubarak Tursynbekov <kuanyshbekuly1002@gmail.com>
2020-12-06 21:21:48 +0600

    upd changes required for heroku upd requirements.txt

--- lemon/settings.py
+++ lemon/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-from secret_key import SECRET_KEY_
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY_
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+# False if not in os.environ
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://lemonback.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['lemonback.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
